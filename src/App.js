41b121267d4ca6fbb4a7d9c1aa8dfe8715897888
ilian.iliev <ilian.iliev@imperiaonline.org>
2021-03-31 13:36:23 +0300

    Added addItem and removeItem functionality

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors';
+
 
 class App extends React.Component {
 
@@ -47,6 +51,7 @@ class App extends React.Component {
                 <Switch> 
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
+                    <Route exact path='/checkout' component={CheckoutPage} />
                     <Route 
                         exact 
                         path='/signin' 
